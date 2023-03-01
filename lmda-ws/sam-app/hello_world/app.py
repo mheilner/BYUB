084a5ea409fe5a67aa0f1f8873763658180ca71a
mheilner <mark.heilner4@gmail.com>
2023-03-01 09:12:36 -0700

    aws training

---
+++ lmda-ws/sam-app/hello_world/app.py
@@ -0,0 +1,18 @@
+import json
+import random
+
+def lambda_handler(event, context):
+
+    # Pick a number between 0 and 200.
+    # If the value is zero, raise an Exception.
+    val = random.randrange(0, 200)
+    if val == 0:
+        raise Exception("something went wrong!")
+
+    # Return a response to the API Gateway
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world"
+        }),
+    }
